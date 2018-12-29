1bbb361d6a6250fa51742433cbf3e3c30fe11d7f
hanguozhi <hanguozhi@bestcode.com>
2018-12-29 18:26:47 +0800

    front

---
+++ example/mainlist.js
@@ -0,0 +1,168 @@
+export default {
+    "date": "2018-12-27",
+    "toast": "",
+    "new_recommend_count_str": "",
+    "recommend_feeds": [{
+        "source_cn": "",
+        "feedback_reason": "",
+        "layout": 1,
+        "target": {
+            "cover_watermark_icon": "",
+            "photos_count": 0,
+            "author": {
+                "verify_type": 4,
+                "avatar": "https://img3.doubanio.com\/icon\/u16021478-100.jpg",
+                "name": "\u5b59\u884d"
+            },
+            "url": "https:\/\/www.douban.com\/note\/701345792\/",
+            "uri": "https:\/\/m.douban.com\/note\/701345792\/",
+            "more_pic_urls": [],
+            "cover_url": "",
+            "cover_watermark_text": "",
+            "kind": 1015,
+            "read_count": 5001,
+            "id": "701345792",
+            "desc": "\u529e\u5065\u8eab\u5361\u4e4b\u524d\uff0c\u6211\u662f\u601d\u8651\u518d\u4e09\u7684\uff0c\u56e0\u4e3a\u542c\u591a\u4e86\u73b0\u5728\u7684\u5065\u8eab\u623f\u90fd\u662f\u4e00\u5e2e\u4e2d\u8001\u5e74\u4eba\u5728\u91cc\u9762\u5404\u81ea\u5360\u5c71\u5934\uff0c\u4e5f\u4e0d\u6b63\u7ecf\u953b\u70bc\uff0c\u7eaf\u7cb9\u5c31\u662f\u4e3a\u4e86\u6d17\u6fa1\u65b9\u4fbf\u3002 \u8fd8\u6709\u4eba\u715e\u6709\u4ecb\u4e8b\u5730\u8bf4\uff0c\u50cf\u8fd9\u4e9b\u6cb9\u817b\u7684\u4e2d\u8001\u5e74\u4eba\u662f\u5728\u5bb6\u91cc\u5446\u4e0d\u4f4f\u7684\uff0c\u4f60\u60f3\u554a\uff0c\u5927\u5a18\u5927\u5988\u4eec\u90fd\u53bb\u8df3\u5e7f\u573a\u821e\u4e86\uff0c\u4e5f\u6ca1\u4eba\u642d\u7406\u4ed6\u4eec\uff0c\u7b49\u5230\u4e86\u6d17\u6fa1\u65f6\u95f4\uff0c\u5927\u5a18\u5927\u5988\u4eec\u53c8\u56de\u6765\u4e86\uff0c\u4e00\u626d\u5934\uff0c\u56af\uff0c\u8fd8\u5728\u90a3\u91cc\u6775\u7740\u5462\uff0c\u6211\u5f97\u6d17\u6fa1\u4e86\uff0c\u4f60\u8be5\u4e0a\u54ea\u6b47\u7740\u6b47\u7740\u53bb\u5427\u3002\u4f60\u60f3\uff0c\u5927\u7237\u5927\u53d4\u4eec\u54ea\u53d7\u5f97\u4e86\u8fd9\u5520\u53e8\u554a\uff0c\u5e72\u8106\u529e\u5f20\u5065\u8eab\u5361\u5f97\u4e86\uff0c\u8fd8\u53ef\u4ee5\u7ec4\u4e2a\u56e2\u804a\u4e2a\u5929\u3002 \u5c31\u8fde\u5065\u8eab\u4e2d\u5fc3\u670d\u52a1\u53f0\u7684\u5c0f\u59b9\u513f\u5728\u6211\u529e\u5361\u7684\u65f6\u5019\u90fd..."
+        },
+        "title": "\u4e2d\u8001\u5e74\u5065\u8eab\u623f\u5947\u9047\u8bb0",
+        "source": "hot",
+        "theme": null,
+        "id": "160926",
+        "app_event_supplementary_info": null
+    }, {
+        "source_cn": "",
+        "feedback_reason": "",
+        "layout": 1,
+        "target": {
+            "cover_watermark_icon": "",
+            "photos_count": 0,
+            "author": {
+                "verify_type": 4,
+                "avatar": "https://img3.doubanio.com\/icon\/u3218526-10.jpg",
+                "name": "\u767d\u5173"
+            },
+            "url": "https:\/\/www.douban.com\/note\/701301640\/",
+            "uri": "https:\/\/m.douban.com\/note\/701301640\/",
+            "more_pic_urls": [],
+            "cover_url": "https://img3.doubanio.com\/view\/note\/sqs\/public\/p56811374.jpg",
+            "cover_watermark_text": "",
+            "kind": 1015,
+            "read_count": 2364,
+            "id": "701301640",
+            "desc": "\u5341\u5e74\u524d\uff0c\u6211\u662f\u4e00\u4f4d\u7acb\u8a93\u753b\u539f\u521b\u6f2b\u753b\u7684\u72c2\u5984\u5c0f\u9547\u9752\u5e74\u3002 \u73b0\u5728\uff0c\u6211\u662f\u6b63\u5728\u88ab\u753b\u6f2b\u753b\u8fd9\u4e8b\u8e42\u8e8f\u6467\u6b8b\u8fd8\u6267\u8ff7\u4e0d\u609f\u7684\u4e2d\u5e74\u5927\u53d4\u3002 \u5341\u5e74\u524d\uff0c\u7f51\u7edc\u59cb\u5174\uff0c\u6240\u6709\u7684\u7f51\u7ad9\u90fd\u4f7f\u51fa\u6d51\u8eab\u89e3\u6570\u5bf9\u7740\u4eba\u5f80\u5916\u5472\u5e7f\u544a\uff0c\u65e0\u610f\u4e2d\u53d1\u73b0\u4e00\u4e2a\u770b\u4e0a\u53bb\u7b80\u6d01\u6e05\u6f88\u4eba\u755c\u65e0\u5bb3\u7684\u7f51\u7ad9\u3002\u4e00\u4e0b\u5c31\u88ab\u5438\u5f15\u6ce8\u518c\u3002 \u5230\u5982\u4eca\uff0c\u4f60\u65b9\u5531\u7f62\u6211\u767b\u573a\uff0c\u6709\u7684\u6210\u4e86\u9738\u4e3b\uff0c\u6709\u7684\u70df\u6d88\u4e91\u6563.....\u4e00\u76f4\u671f\u5f85\u4ed6\u4e0d\u53d8\u7684\uff0c\u4ed6\u679c\u7136\u4e5f\u6ca1\u600e\u4e48\u53d8\u7684\uff08\u6ca1\u53d8\u6210\u529f\uff09\uff0c\u5e94\u8be5\u5c31\u53ea\u6709\u8c46\u74e3\u4e86\u5427\uff1f 2018\u5e74\u773c\u770b\u5c31\u8981\u8fc7\u53bb\uff0c\u672c\u6765\u662f\u60f3\u5199\u4e00\u5199\u4e00\u5e74\u7684\u603b\u7ed3\u3002\u5750\u5230\u7535\u8111\u524d\uff0c\u6ca1\u6709\u5934\u7eea\u3002\u4e0b\u610f\u8bc6\u987a\u624b\u70b9\u5f00\u4e86\u8c46\u74e3\u3002\u6620\u5165..."
+        },
+        "title": "\u6211\u4e0a\u8c46\u74e3\u5341\u5e74",
+        "source": "hot",
+        "theme": null,
+        "id": "160928",
+        "app_event_supplementary_info": null
+    }, {
+        "source_cn": "",
+        "feedback_reason": "",
+        "layout": 1,
+        "target": {
+            "cover_watermark_icon": "",
+            "photos_count": 0,
+            "author": {
+                "verify_type": 4,
+                "avatar": "https://img3.doubanio.com\/icon\/u3103758-24.jpg",
+                "name": "\u5317\u5b89"
+            },
+            "url": "https:\/\/www.douban.com\/note\/701378076\/",
+            "uri": "https:\/\/m.douban.com\/note\/701378076\/",
+            "more_pic_urls": [],
+            "cover_url": "https://img3.doubanio.com\/view\/note\/sqs\/public\/p56835572.jpg",
+            "cover_watermark_text": "",
+            "kind": 1015,
+            "read_count": 5424,
+            "id": "701378076",
+            "desc": "\u591a\u591a\u7684\u51fa\u751f\u53ef\u8c13\u4e00\u6b21\u5076\u7136\uff0c\u90a3\u65f6\u6211\u5b8c\u5168\u6ca1\u6709\u60f3\u8fc7\u8981\u5b69\u5b50\u3002\u548c\u5973\u53cb\uff08\u5982\u4eca\u7684\u8001\u5a46\uff09\u53bb\u6842\u6797\u65c5\u884c\uff0c\u53ef\u80fd\u7956\u56fd\u7684\u98ce\u666f\u592a\u8fc7\u7f8e\u597d\u7ed9\u4e86\u6211\u4eec\u65e0\u9650\u7684\u6fc0\u60c5\uff0c\u51b2\u52a8\u4e4b\u4e0b\u5fd8\u8bb0\u505a\u5b89\u5168\u63aa\u65bd\uff0c\u56de\u6765\u540e\u5973\u53cb\u6000\u5b55\u4e86\u3002 \u4e8e\u662f\u5f00\u59cb\u9886\u8bc1\u4e70\u623f\u51c6\u5907\u8fce\u63a5\u4e00\u4e2a\u65b0\u751f\u547d\u7684\u964d\u4e34\uff0c\u5f53\u65f6\u6211\u8fd8\u4e00\u5ea6\u60f3\u7ed9\u672a\u51fa\u751f\u7684\u5b69\u5b50\u8d77\u540d\u53eb\u6842\u6797\uff0c\u60f3\u7740\u4ee5\u540e\u4e5f\u8bb8\u80fd\u62e5\u6709\u4e00\u756a\u7532\u5929\u4e0b\u7684\u4eba\u751f\uff0c\u4f46\u60f3\u60f3\u81ea\u5df1\u7684\u5e73\u5eb8\uff0c\u6700\u540e\u8fd8\u662f\u4ece\u675c\u5de5\u90e8\u7684\u8bd7\u53e5\u91cc\u627e\u4e86\u4e24\u4e2a\u5b57\u3002\u5e38\u8a00\u9053\u5b69\u5b50\u662f\u4e3a\u4e86\u6298\u817e\u7236\u6bcd\u624d\u6765\u5230\u4e16\u4e0a\u7684\uff0c\u6b64\u8bdd\u4e0d\u5047\uff0c\u4ed6\u51fa\u751f\u5c31\u7ed9\u4e86\u6211\u4e00\u756a\u6298\u817e\u3002 \u79bb\u9884\u4ea7\u671f\u8fd8\u6709\u5341\u591a\u5929\uff0c\u516c\u53f8\u8981\u53bb\u6d77\u8fb9\u641e..."
+        },
+        "title": "\u591a\u591a\uff0c\u8c22\u8c22\u4f60",
+        "source": "hot",
+        "theme": null,
+        "id": "160932",
+        "app_event_supplementary_info": null
+    }, {
+        "source_cn": "",
+        "feedback_reason": "",
+        "layout": 1,
+        "target": {
+            "cover_watermark_icon": "",
+            "photos_count": 0,
+            "author": {
+                "verify_type": 4,
+                "avatar": "https://img1.doubanio.com\/icon\/u1062540-27.jpg",
+                "name": "\u6728\u5b50\u5c0f\u9c7c"
+            },
+            "url": "https:\/\/www.douban.com\/note\/701318786\/",
+            "uri": "https:\/\/m.douban.com\/note\/701318786\/",
+            "more_pic_urls": [],
+            "cover_url": "https://img3.doubanio.com\/view\/note\/sqs\/public\/p56823376.jpg",
+            "cover_watermark_text": "",
+            "kind": 1015,
+            "read_count": 1300,
+            "id": "701318786",
+            "desc": "\u5fae\u4fe1\u516c\u4f17\u53f7\uff1a\u6728\u5b50\u5c0f\u9c7c\u624b\u7ed8 \u968f\u624b\u62cd\u4e00\u4e9b\u9732\u53f0\u7684\u56db\u5b63\uff0c\u5df2\u662f\u591a\u5e74\u7684\u4e60\u60ef\u3002\u6bcf\u4e00\u5e74\u7684\u690d\u7269\u76f8\u518c\uff0c\u4e5f\u662f\u6709\u5404\u79cd\u7167\u7247\u3002\u60f3\u6765\u8fd8\u662f\u6574\u7406\u4e00\u7bc7\uff0c2018\u5e74\u7684\u9732\u53f0\u690d\u7269\u8bb0\u3002\u56db\u5b63\u6d41\u8f6c\uff0c\u751f\u751f\u4e0d\u606f\u3002\u81f4\u5c06\u8981\u8fc7\u53bb\u76842018\u5e74\u3002 \u613f\u6d41\u5e74\u53ef\u5fc6\uff0c\u672a\u6765\u53ef\u671f\u3002 \u4e00\u6708\uff1a\u8721\u6885\u5f00\u4e86\u4e5f\u6709\u4e00\u5468\u3002\u4ece\u6700\u521d\u7684\u4e24\u6735\u5230\u73b0\u5728\u7684\u534a\u6570\u7efd\u5f00\uff0c\u51ac\u5929\u7684\u9999\u6c14\u5728\u4e00\u6708\u6e05\u51b7\u7684\u7a7a\u6c14\u91cc\u6563\u5f00\u3002 \u4e8c\u6708\uff1a\u5728\u5bcc\u8d35\u7c7d\u7684\u559c\u5e86\u6c14\u606f\u91cc\uff0c\u8fce\u6765\u4e86\u519c\u5386\u65b0\u5e74\u3002 \u5929\u6c14\u6674\u597d\u7684\u4e8c\u6708\u672b\uff0c\u9633\u5149\u7167\u5728\u9732\u53f0\u3002\u5149\u5f71\u4e4b\u95f4\uff0c\u91d1\u6854\u7f8e\u4e86\uff0c\u8537\u8587\u957f\u4e86\u65b0\u53f6\u3002 \u4e09\u6708\uff1a\u82b1\u67b6\u4e0a\u7684\u8537\u8587\u957f\u5f97\u98de\u5feb\uff0c\u8fd8\u6253\u4e86\u82b1\u82de\u3002\u94fa\u5929\u76d6..."
+        },
+        "title": "2018\uff0c\u9732\u53f0\u7684\u56db\u5b63\u3002",
+        "source": "editor",
+        "theme": null,
+        "id": "160927",
+        "app_event_supplementary_info": null
+    }, {
+        "source_cn": "",
+        "feedback_reason": "",
+        "layout": 1,
+        "target": {
+            "cover_watermark_icon": "",
+            "photos_count": 0,
+            "author": {
+                "verify_type": 0,
+                "avatar": "https://img1.doubanio.com\/icon\/u1867648-158.jpg",
+                "name": "\u6731\u5cb3"
+            },
+            "url": "https:\/\/www.douban.com\/note\/701393707\/",
+            "uri": "https:\/\/m.douban.com\/note\/701393707\/",
+            "more_pic_urls": [],
+            "cover_url": "",
+            "cover_watermark_text": "",
+            "kind": 1015,
+            "read_count": 4642,
+            "id": "701393707",
+            "desc": "\u6211\u4eec\u5206\u5230\u4e86\u571f\u5730 \u2014\u2014\u6f2b\u8c08\u539f\u521b\u6587\u5b66 \u6731\u5cb3 \u5c31\u6211\u81ea\u5df1\u7684\u9605\u8bfb\u800c\u8a00\uff0c\u5bf9\u4e8e\u6587\u5b66\uff0c\u6211\u6700\u65e9\u5173\u6ce8\u7684\u51e0\u4e4e\u90fd\u662f\u7ffb\u8bd1\u5f15\u8fdb\u7684\u4f5c\u54c1\u3002\u6211\u8bb0\u5f97\uff0c\u521a\u4e0a\u5927\u5b66\u4e0d\u4e45\uff0c\u6211\u548c\u4e00\u4e2a\u9ad8\u4e2d\u540c\u5b66\u76f8\u7ea6\u53bb\u901b\u4e66\u5e97\uff0c\u4e70\u56de\u4e86\u300a\u535a\u5c14\u8d6b\u65af\u5168\u96c6\u300b\u3002\u4e5f\u662f\u8fd9\u4f4d\u540c\u5b66\u5411\u6211\u63a8\u8350\u4e86\u7231\u4f26\u00b7\u5761\u7684\u201c\u9ed1\u732b\u201d\uff0c\u540e\u6765\uff0c\u6211\u5c31\u53c8\u4e70\u4e86\u9ed1\u76ae\u7cbe\u88c5\u7684\u300a\u7231\u4f26\u00b7\u5761\u96c6\u300b\u3002\u6211\u81f3\u4eca\u8fd8\u80fd\u5fc6\u8d77\u62ce\u7740\u4e24\u5377\u6c89\u7538\u7538\u7684\u6587\u96c6\u8fc7\u4e00\u6761\u5927\u9a6c\u8def\u7684\u60c5\u666f\u3002\u540c\u6837\u4e5f\u8bb0\u5f97\uff0c\u6211\u4ece\u660a\u6d77\u697c\u4e70\u4e86\u4e00\u5957\u300a\u82a5\u5ddd\u9f99\u4e4b\u4ecb\u5168\u96c6\u300b\uff0c\u5192\u7740\u5bd2\u98ce\u5411\u4e07\u5723\u4e66\u56ed\u8d70\u53bb\u7684\u60c5\u666f\u3002\u8fd8\u6709\u5e38\u4f34\u6211\u5de6\u53f3\u7684\u300a\u5361\u592b\u5361\u5168\u96c6\u300b\uff0c\u90a3\u4e00\u6392\u9634\u90c1\u7684\u5361\u592b\u5361..."
+        },
+        "title": "\u6211\u4eec\u5206\u5230\u4e86\u571f\u5730",
+        "source": "editor",
+        "theme": null,
+        "id": "160933",
+        "app_event_supplementary_info": null
+    }, {
+        "source_cn": "",
+        "feedback_reason": "",
+        "layout": 1,
+        "target": {
+            "cover_watermark_icon": "",
+            "photos_count": 0,
+            "author": {
+                "verify_type": 0,
+                "avatar": "https://img3.doubanio.com\/icon\/u161147127-2.jpg",
+                "name": "\u65b0\u4eac\u62a5\u4e66\u8bc4\u5468\u520a"
+            },
+            "url": "https:\/\/www.douban.com\/note\/701380738\/",
+            "uri": "https:\/\/m.douban.com\/note\/701380738\/",
+            "more_pic_urls": [],
+            "cover_url": "https://img1.doubanio.com\/view\/note\/sqs\/public\/p56835968.jpg",
+            "cover_watermark_text": "",
+            "kind": 1015,
+            "read_count": 1924,
+            "id": "701380738",
+            "desc": "\u5173\u4e8e\u4e8c\u6218\u4e0e\u7eb3\u7cb9\u5fb7\u56fd\uff0c\u5386\u53f2\u5b66\u5bb6\u7684\u8457\u4f5c\u65e9\u5df2\u6c57\u725b\u5145\u680b\u3002\u4f46\u8fd8\u662f\u6709\u4eba\u5728\u5176\u4e2d\u53d1\u73b0\u4e86\u201c\u65b0\u9c9c\u4e8b\u201d\u3002 \u4e00\u6218\u540e\u5bfc\u81f4\u7684\u793e\u4f1a\u521b\u4f24\uff0c\u8ba9\u5fb7\u56fd\u6c11\u4f17\u60c5\u7eea\u4f4e\u8ff7\uff0c\u9762\u5bf9\u6218\u8d25\u7684\u6df1\u91cd\u521b\u4f24\uff0c\u65e0\u8bba\u662f\u8eab\u4f53\u8fd8\u662f\u7cbe\u795e\uff0c\u90fd\u6025\u9700\u67d0\u79cd\u632f\u594b\u4eba\u5fc3\u7684\u65b0\u80fd\u91cf\u3002\u9b4f\u739b\u65f6\u4ee3\u7684\u4eba\u4eec\u62b1\u6028\u4e0d\u5c3d\u5982\u4eba\u610f\u7684\u793e\u4f1a\u73b0\u5b9e\uff0c\u501f\u52a9\u6bd2\u54c1\u8eb2\u8fdb\u8ff7\u5e7b\u7684\u4e16\u754c\u3002\u6070\u9022\u5fb7\u56fd\u5236\u836f\u4e1a\u8fc5\u901f\u5d1b\u8d77\uff0c\u5404\u7c7b\u81f4\u5e7b\u5242\u548c\u6bd2\u54c1\u5408\u5242\u88ab\u836f\u54c1\u516c\u53f8\u751f\u4ea7\u51fa\u6765\uff0c\u4ee5\u751f\u9022\u5176\u65f6\u7684\u59ff\u6001\u6d41\u5165\u4e86\u9b4f\u739b\u5fb7\u56fd\u7684\u5343\u5bb6\u4e07\u6237\uff0c\u67cf\u6797\u6210\u4e3a\u7eb5\u60c5\u4eab\u4e50\u4e4b\u57ce\u3002 \u7eb3\u7cb9\u4e0a\u53f0\u4e4b\u540e\uff0c\u4e0d\u4ec5\u9881\u5e03\u4e86\u76f8\u5173\u7684\u7981\u6bd2\u653f\u7b56\uff0c\u8fd8\u8fdb\u884c\u4e86\u793e\u4f1a\u6027\u7684\u7981\u6bd2..."
+        },
+        "title": "\u7eb3\u7cb9\u3001\u6bd2\u54c1\u548c\u5e0c\u7279\u52d2\u7684\u79c1\u4eba\u533b\u751f\u4e28\u8bfa\u66fc\u00b7\u5965\u52d2\u4e13\u8bbf",
+        "source": "editor",
+        "theme": null,
+        "id": "160931",
+        "app_event_supplementary_info": null
+    }]
+}
